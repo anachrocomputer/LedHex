b9f9cd944e9d562f550045c7e1cb0aeef271f5bf
John Honniball <coredump@virginmedia.com>
2018-02-18 01:57:10 +0000

    Clean up counter code, add comments, modify for new coding standards.

--- LedHexCount4/LedHexCount4.ino
+++ LedHexCount4/LedHexCount4.ino
@@ -1,15 +1,27 @@
-/* LedHexTest --- test the TI TLC5916 with a few LEDs    2014-08-29 */
+/* LedHexCount4 --- display a four-digit counter in hex   2014-08-29 */
+/* Copyright (c) 2014 John Honniball                                 */
 
-#define LE_PIN 10
-#define SDA_PIN 11
-#define SCK_PIN 13
+#define NDIGITS    (4)   // This program uses a four-digit display
+
+// Pin connections from LED board to Arduino board
+// 1 VLED +5V (LED power, OK to use Arduino +5V for a single digit)
+// 2 VCC  +5V (TLC5916 chip power)
+// 3 SDI  D11 (Serial Data In on TLC5916)
+// 4 SDO  no connection (Serial Data out on TLC5916, unused)
+// 5 LE   D10 (Latch Enable on TLC5916)
+// 6 OE   GND (Output Enable, unused in this test, GND to enable)
+// 7 SCK  D13 (Serial Clock to TLC5916)
+// 8 GND  GND (Ground to both LEDs and chip)
+//
+#define LE_PIN 10   // Arduino digital pin 10
+#define SDA_PIN 11  // Arduino digital pin 11
+#define SCK_PIN 13  // Arduino digital pin 13
 /* OE pin grounded */
 
-#define NDIGITS  (4)
-
+// 'F' is #defined as a macro by Arduino framework. Use an int instead
 const int F = (1 << 14);
 
-// Usual 7-segment names for SAA1064
+// Usual 7-segment names
 #define A  (1 << 8)
 #define B  (1 << 0)
 #define C  (1 << 2)
@@ -19,7 +31,7 @@ const int F = (1 << 14);
 #define G  (1 << 10)
 #define DP (1 << 12)
 
-// Additional dot names for HDSP0761
+// Additional dot names
 #define H (1 << 1)
 #define I (1 << 3)
 #define J (1 << 9)
@@ -28,13 +40,15 @@ const int F = (1 << 14);
 #define M (1 << 13)
 #define COLON (1 << 5)
 
+// LED layout on PCB:
+//
 //     H C C I
 //     B     D
 //     B     D
 //     J G G K
 //     A     E
 //     A     E
-//     L F F M
+//     L F F M DP
 
 // Table of 14-segment digits 0-9 and A-F
 const unsigned int HDSPsegtab[16] = {
@@ -57,25 +71,26 @@ const unsigned int HDSPsegtab[16] = {
 };
 
 
-void setup (void)
+void setup(void)
 {
-  pinMode (LE_PIN, OUTPUT);
-  pinMode (SDA_PIN, OUTPUT);
-  pinMode (SCK_PIN, OUTPUT);
+  pinMode(LE_PIN, OUTPUT);
+  pinMode(SDA_PIN, OUTPUT);
+  pinMode(SCK_PIN, OUTPUT);
   
-  digitalWrite (LE_PIN, LOW);
-  digitalWrite (SCK_PIN, LOW);
+  digitalWrite(LE_PIN, LOW);
+  digitalWrite(SCK_PIN, LOW);
 }
 
-void loop (void)
+
+void loop(void)
 {
   int d;
   static unsigned int counter = 0;
   char hexstr[8];
   unsigned int digit;
-  unsigned int segs[NDIGITS];
+  unsigned int segs[NDIGITS];           // 16-bit unsigned binary
   
-  snprintf (hexstr, sizeof (hexstr), "%04x", counter);
+  snprintf(hexstr, sizeof (hexstr), "%04x", counter);
   
   for (d = 0; d < NDIGITS; d++) {
     if (hexstr[d] > '9')
@@ -83,45 +98,54 @@ void loop (void)
     else
       digit = hexstr[d] - '0';
       
-    segs[d] = HDSPsegtab[digit];
+    segs[d] = HDSPsegtab[digit];   // Pick up segment pattern from table
   }
     
-  LedHex_send (segs, NDIGITS);
+  LedHex_send(segs, NDIGITS);     // Send to displays
   
   counter++;
   
-  delay (500);
+  delay(500);                     // Wait half a second
 }
 
-void LedHex_send (unsigned int leds[], int ndig)
+
+/* LedHex_send --- send 'ndig' 16-bit patterns to the LED driver chip */
+
+void LedHex_send(const unsigned int leds[], const int ndig)
 {
+  // Straightforward implementation using digitalWrite(). Could be made
+  // faster by using direct port I/O or SPI hardware.
   int i;
   int d;
   
-  digitalWrite (LE_PIN, LOW);
-  digitalWrite (SCK_PIN, LOW);
-  
+  digitalWrite(LE_PIN, LOW);
+  digitalWrite(SCK_PIN, LOW);
+
+  // Send 16 bits to each pair of TLC5916 chips per digit
   for (d = 0; d < ndig; d++) {
     for (i = 0; i < 16; i++) {
+      // Send one bit on SDA pin
       if (leds[d] & (1u << i))
-        digitalWrite (SDA_PIN, HIGH);
+        digitalWrite(SDA_PIN, HIGH);
       else
-        digitalWrite (SDA_PIN, LOW);
+        digitalWrite(SDA_PIN, LOW);
       
-      delayMicroseconds (1);
+      delayMicroseconds(1);
+
+      // One microsecond HIGH pulse on SCK to clock the bit along
+      digitalWrite(SCK_PIN, HIGH);
       
-      digitalWrite (SCK_PIN, HIGH);
+      delayMicroseconds(1);
       
-      delayMicroseconds (1);
-      
-      digitalWrite (SCK_PIN, LOW);  
+      digitalWrite(SCK_PIN, LOW);  
     }
   }
+
+  // One microsecond HIGH pulse on LE to latch all bits into all the digits
+  digitalWrite(LE_PIN, HIGH);
   
-  digitalWrite (LE_PIN, HIGH);
-  
-  delayMicroseconds (1);
+  delayMicroseconds(1);
     
-  digitalWrite (LE_PIN, LOW);
+  digitalWrite(LE_PIN, LOW);
 }
 
